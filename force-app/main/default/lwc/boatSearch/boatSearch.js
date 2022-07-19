ead1ba1c4ca78773b288c5f5ea91c24b38521ea6
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2022-07-19 10:26:21 -0300

    Criado componente boatSearch com sua estrutura inicial.

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,22 @@
+import { LightningElement, api } from 'lwc';
+
+ // imports
+ export default class BoatSearch extends LightningElement {
+    @api isLoading = false;
+    
+    // Handles loading event
+    handleLoading() { 
+        this.isLoading = true;
+    }
+    
+    // Handles done loading event
+    handleDoneLoading() {
+        this.isLoading = false;
+    }
+    
+    // Handles search boat event
+    // This custom event comes from the form
+    searchBoats(event) { }
+    
+    createNewBoat() { }
+  }
