8a1a09380dc2cee5b2261a351c251910208f00a3
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2022-07-19 13:56:24 -0300

    Criados components boatSearchResult e boatTile.

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -7,20 +7,18 @@ import { LightningElement, api } from 'lwc';
     // Handles loading event
     handleLoading() { 
         this.isLoading = true;
-        setTimeout(() => {
-            this.handleDoneLoading();
-        }, 10000);
     }
     
     // Handles done loading event
     handleDoneLoading() {
-        console.log('teste');
         this.isLoading = false; 
     }
     
     // Handles search boat event
     // This custom event comes from the form
-    searchBoats(event) { }
+    searchBoats(event) {
+        console.log('Id tipo Bot: '+ event.detail );
+    }
     
     createNewBoat() { }
   }
