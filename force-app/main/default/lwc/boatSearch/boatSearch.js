1379a6e4ac7eac43de647674ac6c390a0dc043a8
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2022-07-19 11:35:44 -0300

    adicionado timeout de teste nas funções.

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -7,11 +7,15 @@ import { LightningElement, api } from 'lwc';
     // Handles loading event
     handleLoading() { 
         this.isLoading = true;
+        setTimeout(() => {
+            this.handleDoneLoading();
+        }, 10000);
     }
     
     // Handles done loading event
     handleDoneLoading() {
-        this.isLoading = false;
+        console.log('teste');
+        this.isLoading = false; 
     }
     
     // Handles search boat event
