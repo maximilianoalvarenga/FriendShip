cb59aa99a81effb13a28c625161cc4a415e577cb
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2022-07-22 11:29:52 -0300

    Finalizado step08.

--- force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
+++ force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -1,3 +1,88 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
+import getBoatsByLocation from '@salesforce/apex/BoatDataService.getBoatsByLocation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class BoatsNearMe extends LightningElement {}
+// imports
+const LABEL_YOU_ARE_HERE = 'You are here!';
+const ICON_STANDARD_USER = 'standard:user';
+const ERROR_TITLE = 'Error loading Boats Near Me';
+const ERROR_VARIANT = 'error';
+export default class BoatsNearMe extends LightningElement {
+    @api boatTypeId;
+    @track mapMarkers = [];
+    @track isLoading = true;
+    @track isRendered = false;
+    latitude;
+    longitude;
+  
+    // Add the wired method from the Apex Class
+    // Name it getBoatsByLocation, and use latitude, longitude and boatTypeId
+    // Handle the result and calls createMapMarkers
+    @wire(getBoatsByLocation, { latitude: '$latitude', longitude: '$longitude', boatTypeId: '$boatTypeId'})
+    wiredBoatsJSON({error, data}) {
+        if (data) {
+            this.createMapMarkers(data);
+        } else if (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: ERROR_TITLE,
+                    message: error.body.message,
+                    variant: ERROR_VARIANT
+                })
+            );
+        this.isLoading = false;
+        }
+    }
+    
+    // Controls the isRendered property
+    // Calls getLocationFromBrowser()
+    renderedCallback() {
+        if (this.isRendered == false) {
+            this.getLocationFromBrowser();
+        }
+        this.isRendered = true;
+    }
+    
+    // Gets the location from the Browser
+    // position => {latitude and longitude}
+    getLocationFromBrowser() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+        });
+        this.isLoading = false;
+    }
+    
+    // Creates the map markers
+    createMapMarkers(boatData) {
+        const newBoatData = JSON.parse(boatData);
+        /*this.mapMarkers = [{
+            
+        }];
+        
+
+        //this.mapMarkers.push(newMakers);
+        console.log(this.mapMarkers);
+        this.isLoading = false;*/
+
+        this.mapMarkers = newBoatData.map((item) => {
+            return {
+                location: {
+                    Latitude: item.Geolocation__Latitude__s,
+                    Longitude: item.Geolocation__Longitude__s,
+                },
+                title: item.Name,
+            }
+        });
+        this.mapMarkers.unshift({
+            location: {
+                Latitude: this.latitude,
+                Longitude: this.longitude,
+            },
+            title: LABEL_YOU_ARE_HERE,
+            icon: ICON_STANDARD_USER
+        });
+
+        this.isLoading = false;
+    }
+}
